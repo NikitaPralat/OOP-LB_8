3f6d49bc07eaada322547c5eafce34d747b2b306
Taras Klimarchuk <klimarchuktaras@ukr.net>
2020-11-29 12:27:48 +0200

    Добавьте файлы проекта.

---
+++ 2-task/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace _2_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Напишите дату откуда начнется отчет");
+
+                Console.WriteLine("Напишите месяц от 1 до 12");
+                int month = Convert.ToInt32(Console.ReadLine());
+                while (month < 1 || month > 12)
+                {
+                    Console.WriteLine("Напишите месяц от 1 до 12");
+                    month = Convert.ToInt32(Console.ReadLine());
+                }
+
+                Console.WriteLine("Напишите день с 1 до 30");
+                int day = Convert.ToInt32(Console.ReadLine());
+                while (day < 1 || day > 30)
+                {
+                    Console.WriteLine("Напишите день с 1 до 30");
+                    day = Convert.ToInt32(Console.ReadLine());
+                }
+
+                string date = $"{nToString(day)}.{nToString(month)}";
+
+                DateWork dateWork = new DateWork(day, month, date);
+
+                Console.WriteLine($"Напишите день каторый будет добавлен к дате {date}");
+                int index = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(dateWork[index]);
+            }
+            catch
+            {
+                Console.WriteLine("Значение должно быть числом");
+            }
+        }
+        static string nToString(int n)
+        {
+            if (n < 10)
+                return $"0{n}";
+            else
+                return $"{n}";
+        }
+    }
+    
+    class DateWork
+    {
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public string DateFrom { get; set; }
+        public DateWork(int day, int month, string date)
+        {
+            DateFrom = date;
+            Day = day;
+            Month = month;
+        }
+        public string this[int index]
+        {
+            get
+            {
+                
+                int newMonth = (Day + index) / 30 + Month;
+                int newDay = 0;
+                int f = Day + index;
+                if (f - 30 > 0)
+                {
+                    do
+                    {
+                        f -= 30;
+                    }
+                    while (f > 30);
+                    newDay = f;
+                }
+                else
+                {
+                    newDay = Day + index;
+                }
+                
+                return $"{nToString(newDay)}.{nToString(newMonth)}";
+            }
+        }
+
+        private string nToString(int n)
+        {
+            if (n < 10)
+                return $"0{n}";
+            else
+                return $"{n}";
+        }
+    }
+}
---
+++ 3-task/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using _3_task.Classes;
+
+namespace _3_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Выберите массив с которым вы хотите работать, 1 или 2");
+                Console.WriteLine("1: int[]");
+                Console.WriteLine("2: string[]");
+                int number = Convert.ToInt32(Console.ReadLine());
+                if (number == 1) Console.WriteLine(1);
+                while (number != 1 && number != 2)
+                {
+                    Console.WriteLine("только 1 или 2");
+                    number = Convert.ToInt32(Console.ReadLine());
+                }
+                if (number == 1)
+                {
+                    Console.WriteLine("Вы выбралт массив чисел\n");
+                    Console.WriteLine("Вы выбрали массив строк");
+                    int arraySize = Convert.ToInt32(Console.ReadLine());
+                    ArrayWork arrayWork = new ArrayWork(arraySize);
+                    Console.WriteLine($"Размер массива {arraySize}\n");
+                    Console.WriteLine("Напишите индекс массива, чтобы увидеть элемент");
+                    int index = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($"В индексе {index} размещено {arrayWork[index]}\n");
+                    Console.WriteLine("Если вы хотите изменить это значение, напишите новое значение, если не хотите напишите '0'");
+                    int newValue = Convert.ToInt32(Console.ReadLine());
+                    if (newValue != 0)
+                    {
+                        arrayWork[index] = newValue;
+                        Console.WriteLine($"Вы успешно изменили значение в индексе {index} на {arrayWork[index]}");
+                    }
+                }
+                else if (number == 2)
+                {
+                    Console.WriteLine("Вы выбралт массив чисел\n");
+                    Console.WriteLine("Вы выбрали массив строк");
+                    int arraySize = Convert.ToInt32(Console.ReadLine());
+                    ArrayWorkStr arrayWork = new ArrayWorkStr(arraySize);
+                    Console.WriteLine($"Размер массива {arraySize}\n");
+                    Console.WriteLine("Напишите индекс массива, чтобы увидеть элемент");
+                    int index = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($"В индексе {index} размещено {arrayWork[index]}\n");
+                    Console.WriteLine("Если вы хотите изменить это значение, напишите новое значение, если не хотите напишите '0'");
+                    string newValue = Console.ReadLine();
+                    if (newValue != "0")
+                    {
+                        arrayWork[index] = newValue;
+                        Console.WriteLine($"Вы успешно изменили значение в индексе {index} on {arrayWork[index]}");
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Значение должно быть числом");
+            }
+        }
+    }
+
+}
